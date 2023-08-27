118f33d4a8e8145304bfcf3023522dad53dbfc9a
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-08-27 16:07:15 -0700

    generates prompts and svg

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-'not pushing to git hub test'
+const fs = require("fs");
+const inquirerPromise = import('inquirer');
+
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+
+inquirerPromise.then(inquirerModule => {
+  const inquirer = inquirerModule.default;
+
+  const generateLogo = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => {
+        return input.length <= 3 ? true : 'Please enter up to three characters.';
+      },
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (color keyword or hexadecimal number):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (color keyword or hexadecimal number):',
+    },
+  ];
+
+  inquirer.prompt(generateLogo).then((answers) => {
+    const shapeType = answers.shape;
+    const textColor = answers.textColor;
+    const shapeColor = answers.shapeColor;
+
+    const shape = shapeType === 'circle'
+      ? new Circle()
+      : shapeType === 'triangle'
+        ? new Triangle()
+        : new Square();
+
+    shape.setColor(shapeColor);
+
+    const svg = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <text x="50" y="50" fill="${textColor}" font-size="16">${answers.text}</text>
+      ${shape.getSVG()}
+      </svg>
+    `;
+
+    fs.writeFileSync('logo.svg', svg);
+
+    console.log("Generated logo.svg");
+  });
+});
